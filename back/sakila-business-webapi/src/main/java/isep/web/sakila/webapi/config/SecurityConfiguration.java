67656134c95c834a71111b8b8ae72c1e049372bb
Victor Ely <vely@juniorisep.com>
2017-12-29 21:24:55 +0100

    Update conf

--- back/sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/SecurityConfiguration.java
+++ back/sakila-business-webapi/src/main/java/isep/web/sakila/webapi/config/SecurityConfiguration.java
@@ -1,36 +1,88 @@
 package isep.web.sakila.webapi.config;
 
+import java.util.Arrays;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import isep.web.sakila.webapi.security.JWTAuthenticationFilter;
+import isep.web.sakila.webapi.security.JWTAuthorizationFilter;
+import isep.web.sakila.webapi.security.JWTUtil;
 
 @Configuration
-@EnableGlobalMethodSecurity(prePostEnabled = true, jsr250Enabled = true)
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 
-	protected void configure(HttpSecurity http) throws Exception {
-		http
-			.authorizeRequests()
-			.antMatchers("/").permitAll()
-			.anyRequest().authenticated()
-			.and()
-			.formLogin()
-			.loginPage("/login").permitAll()
-			.and()
-			.logout().permitAll()
-			.and()
-			.csrf().disable();
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+	    return super.userDetailsService();
 	}
 
-    @Autowired
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("login").password("password").roles("USER");
-    }
+	@Autowired
+	private UserDetailsService userDetailsService;
+
+	@Autowired
+    private Environment env;
+
+	@Autowired
+	private JWTUtil jwtUtil;
+
+	private static final String[] PUBLIC_MATCHERS = {
+			"/resources/**"
+	};
+
+	private static final String[] PUBLIC_MATCHERS_GET = {
+			"/auth/**"
+	};
+
+	private static final String[] PUBLIC_MATCHERS_POST = {
+			"/auth/refresh_token"
+	};
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+
+
+		http.csrf().disable();
+		http.authorizeRequests()
+			.antMatchers(PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(HttpMethod.POST, PUBLIC_MATCHERS_POST).permitAll()
+			.antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+			.antMatchers(PUBLIC_MATCHERS).permitAll();
+		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
+		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	}
+
+
+	@Bean
+	CorsConfigurationSource corsConfigurationSource() {
+		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration cors = new CorsConfiguration();
+		cors.setAllowCredentials(true);
+		cors.addAllowedOrigin("*");
+		cors.addAllowedHeader("*");
+		cors.addAllowedMethod("OPTIONS");
+		cors.addAllowedMethod("GET");
+		cors.addAllowedMethod("POST");
+		cors.addAllowedMethod("PUT");
+		cors.addAllowedMethod("DELETE");
+		source.registerCorsConfiguration("/**", cors);
+		return source;
+	}
 
 }
